0d4937f98bc30962545fc70ce66fda7078ba91dc
Dnyelfy <98780811+Dnyelfy@users.noreply.github.com>
2026-03-21 23:29:59 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-21T20:29:47.626Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-21T20:29:59.232Z
+Commit: Fix broken links in documentation
