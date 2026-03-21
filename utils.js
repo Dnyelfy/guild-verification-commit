c3e3a7096ff170523c26f646cce2f2fe9469c2a5
Dnyelfy <98780811+Dnyelfy@users.noreply.github.com>
2026-03-21 23:29:49 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-21T20:29:27.649Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-21T20:29:49.303Z
+Commit: Add export functionality for reports
