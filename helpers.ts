c0db3d0421c591f49c97b8363dea8266fe943501
Dnyelfy <98780811+Dnyelfy@users.noreply.github.com>
2026-03-27 16:08:17 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T13:08:13.074Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-27T13:08:16.418Z
+Commit: Fix database connection timeout
