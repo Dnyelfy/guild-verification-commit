b9174b868bc70edf77fefe4af484075060851b27
Dnyelfy <98780811+Dnyelfy@users.noreply.github.com>
2026-03-27 16:07:49 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T13:07:39.770Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-27T13:07:48.876Z
+Commit: Fix memory leak in event handlers
