b1bf49496e2f0d7141a9ed589a3d35b425746d72
Dnyelfy <98780811+Dnyelfy@users.noreply.github.com>
2026-03-27 16:07:40 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T13:07:22.129Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-27T13:07:39.770Z
+Commit: Resolve null pointer exception in user service
